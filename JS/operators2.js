f3b4a42c7d9c2dd8e723dbb693904996dcd9e2bd
Blazequiz <161931733+Blazequiz@users.noreply.github.com>
2025-02-02 14:48:53 +0100

    practice lesson 2

--- JS/operators2.js
+++ JS/operators2.js
@@ -1,14 +1,14 @@
-let a = 5;
-let b = 10;
-let c = 15;
-let d = 20;
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
 
-a += 2;
-b -= 4;
-c *= 3;
-d /= 10;
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
 
-console.log(a, b , c , d);
+// console.log(a, b , c , d);
 
 
 // a = a + 2;
@@ -23,11 +23,11 @@ console.log(a, b , c , d);
 //  Змінна pricePerItem зберігає ціну за одиницю товару,
 //  а orderedQuantity - кількість одиниць товару в замовленні.
 
-const pricePerItem = 3500;
-const orderedQuantity = 4;
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
 
-const totalPrice = pricePerItem * orderedQuantity;
-console.log(totalPrice);
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
 
 
 // 3
@@ -39,9 +39,9 @@ console.log(totalPrice);
 //  значенням isAdult було true, якщо вік користувача більше або дорівнює 18 
 //  та false, якщо менше 18.
 
-const age = 18;
-const isAdult = age >= 18;
-console.log(isAdult)
+// const age = 18;
+// const isAdult = age >= 18;
+// console.log(isAdult)
 
 // 4
 // Оголоси змінну isValid і задай їй значенням результат перевірки
@@ -50,8 +50,44 @@ console.log(isAdult)
 // Якщо паролі співпадають (сувора рівність), значення isValid має бути true
 // Якщо паролі не співпадають, значення isValid має бути false
 
-const correctPassword = "jqueryismyjam";
-const userPassword = "mangodab3st";
+// const correctPassword = "jqueryismyjam";
+// const userPassword = "mangodab3st";
 
-const isValid = correctPassword === userPassword;
-console.log(isValid);
+// const isValid = correctPassword === userPassword;
+// console.log(isValid);
+
+// 5
+
+// const enterFirstNummber = prompt("Vvedite chislo: ")
+// const enterSecondNummber = prompt("Vvedite chislo2: ")
+// console.log(Number(enterFirstNummber) + Number(enterSecondNummber));
+// const summOfNummber = Number(enterFirstNummber) + Number(enterSecondNummber);
+// alert(summOfNummber)
+
+
+// 6
+
+// 5.845   >>>>>   5.8
+// let numOne = 5.845;
+// numOne = numOne.toFixed(1);
+// numOne = Number(numOne);
+// console.log(numOne);
+
+/*   задача 4
+    Напиши скрипт який каже користувачу ввести число та ступінь,
+    потім зводить число до ступеню та виводить результат в консоль
+      1. попросити ввести число і зберегти в змінну та вивести в консоль
+      2. попросити ввести ступінь та зберегти в змінну та вивести в консоль
+      3. Звести введені дані до ступеня та вивести результат в консоль
+
+      !!! перед розрахунком перетворити явно отримані результати в число
+ */
+
+// const enterNumber = prompt("vvedite chislo: ") 
+// console.log(enterNumber);
+// const numberDegree =  prompt("vvedite degree of the number: ") 
+// console.log(numberDegree);
+// const degreeResult = Number(enterNumber) ** Number(numberDegree);
+// console.log(degreeResult);
+
+
