75de3f1ceacda2f793efdd5dff775d68d53adfea
Blazequiz <161931733+Blazequiz@users.noreply.github.com>
2025-04-12 09:36:21 +0200

    lesson 11

--- JS/arrows-10.js
+++ JS/arrows-10.js
@@ -64,3 +64,89 @@ const addLogin = (allLogins, login) => {
 
 console.log(addLogin(allLogins, "ffffff"));
 console.log(allLogins);
+
+// 
+
+// const filter = function (array , callback) {
+//   let filteredNumbers = [];
+//   for ( const number of array) {
+//     const passTest = callback(number);
+//     if(passTest) {
+//       filteredNumbers.push(number)
+//     }
+//   }
+//   return filteredNumbers;
+// }
+// const f1 = function (num) {
+//   return num < 4;
+// }
+
+// const array12 = [1, 2, 4 , 3 , 5 , 6];
+
+// console.log(filter(array12, f1));
+
+// for hw
+
+const filter = (array, test) => {
+  const filteredArray = [];
+
+  for (const el of array) {
+    // console.log(el);
+    const passed = test(el);
+
+    if (passed) {
+      filteredArray.push(el);
+    }
+  }
+
+  return filteredArray;
+};
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// const callback1 = (value) => value >= 3;
+
+const r1 = filter(arr, (value) => value >= 3);
+// console.log(r1);
+
+// const callback2 = (value) => value <= 4;
+
+const r2 = filter(arr, (value) => value <= 4);
+// console.log(r2);
+
+/*   1.
+Напишіть функцію, яка приймає на вхід масив чисел 
+і повертає новий масив, в якому кожний елемент є 
+квадратом відповідного елементу вхідного масиву.
+Знайдіть суму елементів масиву 
+з використанням колбек-функції */
+
+
+
+
+const array4 = [2 , 4 , 5 , 6 , 9 , 12 , 7 , 89 ]
+
+const arrToSquare = function(arr, callback) {
+  let newArray = [];
+  for (const item of arr) {
+    newArray.push(item**2)
+  }
+  callback(newArray)
+}
+
+const f2 = (arr) => {
+  let sumArray = 0;
+  for(const item of arr) {
+    sumArray += item;
+  }
+  return console.log(sumArray);
+} 
+const f3 = (arr) => {
+  let multiplyArray = 1;
+  for(const item of arr) {
+    multiplyArray *= item;
+  }
+  return console.log(multiplyArray);
+} 
+
+
+(arrToSquare(array4, f3));
